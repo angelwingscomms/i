041ce9cc5cf3f1880b8b43636e5948c277163b39
Gold Edem Hogan <edge37@outlook.com>
2025-08-03 21:04:07 +0100

    All Glory Be To God

--- src/routes/r/[i]/+server.ts
+++ src/routes/r/[i]/+server.ts
@@ -2,11 +2,11 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ request, platform, params }) => {
 	const i = platform?.env.R.idFromName(params.i);
-	console.log('r i', i)
+	console.log('r i', i);
 	const o = platform?.env.R.get(i);
-	console.log('r o', o)
+	console.log('r o', o);
 	const res = await o.fetch(new Request(request.url, request));
-	console.log('platform.env', platform.env, Object.entries(platform.env))
+	console.log('platform.env', platform.env, Object.entries(platform.env));
 	console.log('r res', res);
-	return new Response()
+	return { body: res };
 };
