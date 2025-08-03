369163c4146f00b391206dff36aeeadabd4c74ba
Gold Edem Hogan <edge37@outlook.com>
2025-08-03 07:03:18 +0100

    All Glory Be To God

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,39 @@
 import type { Handle } from '@sveltejs/kit';
-import {validateSessionToken, sessionCookieName} from '$lib/server/auth';
+import { dev } from '$app/environment';
+import { validateSessionToken, sessionCookieName } from '$lib/server/auth';
+
+let env = {};
+
+if (dev) {
+	/*
+  We need to import miniflare here conditionally in dev since the esbuild which runs during
+  `npm run build` won't be able to resolve `node:*` imports since they aren't marked as external.
+
+  See the following for more info:
+  - https://github.com/sveltejs/kit/issues/10732
+  - https://github.com/sveltejs/kit/pull/10544
+*/
+	const { Miniflare } = await import('miniflare');
+	const mf = new Miniflare({
+		modules: true,
+		durableObjects: { COUNTER: 'Counter' },
+		durableObjectsPersist: '.wrangler/state/v3/do',
+		scriptPath: 'durable-objects/counter/index.js'
+	});
+
+	env = await mf.getBindings();
+}
 
 const handleAuth: Handle = async ({ event, resolve }) => {
+	if (dev) {
+		event.platform = {
+			...event.platform,
+			env
+		};
+	}
+
 	const sessionToken = event.cookies.get(sessionCookieName);
-  // console.log('st', sessionToken)
+	// console.log('st', sessionToken)
 
 	if (!sessionToken) {
 		return resolve(event);
@@ -17,10 +47,10 @@ const handleAuth: Handle = async ({ event, resolve }) => {
 	// } else {
 	// 	auth.deleteSessionTokenCookie(event);
 	// }
-	// 
-	if (!user) return resolve(event)
+	//
+	if (!user) return resolve(event);
 
-	event.locals.user = {i: user.i, t: user.t};
+	event.locals.user = { i: user.i, t: user.t };
 	event.locals.session = session;
 	return resolve(event);
 };
