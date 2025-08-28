c368585b6da3abc73f5e673c97f463acb9df84e1
Gold Edem Hogan <edge37@outlook.com>
2025-08-28 20:56:54 +0100

    All Glory Be To God - done

--- src/routes/devr/+server.ts
+++ src/routes/devr/+server.ts
@@ -3,13 +3,13 @@ import { error, json } from '@sveltejs/kit';
 import { count, format_filter, qdrant, get } from '$lib/db';
 import { collection } from '$lib/constants';
 import type { User } from '$lib/types';
+import { dev } from '$app/environment';
 
 // Deletes all rooms that are not one-on-one ("|") or anon ("-")
 export const GET: RequestHandler = async ({ locals }) => {
-	if (!locals.user?.i) throw error(401, 'Unauthorized');
-	const me = await get<User>(locals.user.i);
-	if (!me?.isAdmin) throw error(403, 'Forbidden');
-
+	if (!dev) {
+		error(403);
+	}
 	const payloadFilter = { s: 'r', _: { in: { values: [',', '.'] } } } as const;
 	const filter = format_filter(payloadFilter);
 
@@ -22,5 +22,3 @@ export const GET: RequestHandler = async ({ locals }) => {
 
 	return json({ deleted: toDelete });
 };
-
-
