5f239764f92277a7ed663a3ae8ae21c3db25c946
Gold Edem Hogan <edge37@outlook.com>
2025-08-28 20:59:20 +0100

    All Glory Be To God - done

--- src/routes/devr/+server.ts
+++ src/routes/devr/+server.ts
@@ -1,24 +1,29 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { error, json } from '@sveltejs/kit';
-import { count, format_filter, qdrant, get } from '$lib/db';
+import { count, qdrant } from '$lib/db';
 import { collection } from '$lib/constants';
-import type { User } from '$lib/types';
 import { dev } from '$app/environment';
 
 // Deletes all rooms that are not one-on-one ("|") or anon ("-")
-export const GET: RequestHandler = async ({ locals }) => {
+export const GET: RequestHandler = async () => {
 	if (!dev) {
 		error(403);
 	}
-	const payloadFilter = { s: 'r', _: { in: { values: [',', '.'] } } } as const;
-	const filter = format_filter(payloadFilter);
+	// const filter = {
+	// 	must: [
+	// 		{
+	// 			key: 's',
+	// 			match: {
+	// 				value: 'r'
+	// 			}
+	// 		}
+	// 	]
+	// };
 
-	const toDelete = await count(payloadFilter);
+	// await qdrant.delete(collection, {
+	// 	wait: true,
+	// 	filter
+	// });
 
-	await qdrant.delete(collection, {
-		wait: true,
-		filter
-	});
-
-	return json({ deleted: toDelete });
+	return new Response('.');
 };
