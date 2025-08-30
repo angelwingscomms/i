423690841127d548006d884f98e04b16504548ea
Gold Edem Hogan <edge37@outlook.com>
2025-08-30 20:45:21 +0100

    All Glory Be To God - done

--- src/lib/util/cf.ts
+++ src/lib/util/cf.ts
@@ -1,5 +1,7 @@
 import { dev } from '$app/environment';
 
 export const cf = (platform: Readonly<App.Platform> | undefined) => {
+	console.log('platform', platform);
+	console.log('platform.env', platform.env);
 	return dev ? fetch : platform.env.r.fetch;
 };
